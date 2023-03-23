10f7e8c2f0ebe232380958cc82a3f1a51acf00b1
abethefoodguy <128656901+abethefoodguy@users.noreply.github.com>
2023-03-22 23:03:16 -0400

    Create facebookcreds.py

---
+++ facebookcreds.py
@@ -0,0 +1,9 @@
+userName = input ("Hello! Please enter your facebook name!")
+
+userPassword = input ("Hello Please enter your facebook password!")
+
+    if (userName.lower() == "Abe" and userPassword == "ham"):
+            print("welcome to facebook!")
+    else:
+        print("wrong credentials,TRY AGAIN!")
+    
