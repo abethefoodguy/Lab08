fb45ffb385ba818e52aed3cf72a98792966b80a6
abethefoodguy <128656901+abethefoodguy@users.noreply.github.com>
2023-03-22 22:29:58 -0400

    added helloWorld.py

---
+++ helloWorld.py
@@ -0,0 +1,8 @@
+Python 3.11.2 (v3.11.2:878ead1ac1, Feb  7 2023, 10:02:41) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================================================================== RESTART: /Users/abrahamotele/Dropbox/Mac/Documents/helloWorld.py ==================================================================
+Hello World
+>>> 
+================================================================== RESTART: /Users/abrahamotele/Dropbox/Mac/Documents/helloWorld.py ==================================================================
+Hello World,my name is abe and I love Python
